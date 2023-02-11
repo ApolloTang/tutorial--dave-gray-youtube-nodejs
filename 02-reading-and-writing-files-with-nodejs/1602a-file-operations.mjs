5889003fd1c3d37e06bb151c2e4f04ae80982ea1
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-02-11 10:44:57 -0500

    done: 1602a-file-operations.mjs

---
+++ 02-reading-and-writing-files-with-nodejs/1602a-file-operations.mjs
@@ -0,0 +1,39 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import url from 'node:url'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const {
+  readFile,
+  writeFile,
+  appendFile,
+  rename,
+  unlink,
+} = fs.promises
+
+
+const filenameWithPath = path.join(__dirname, 'files', 'created-text-file3.txt')
+const newFilenameWithPath = path.join(__dirname, 'files', 'created-text-file4.txt')
+const content = 'new content\n'
+
+const fileOps = async () => {
+  try  {
+    await unlink(newFilenameWithPath).catch( err => {
+      // 'unlink' means deletion, it will thow if file does not exist
+      console.log(err)
+    })
+    await writeFile(filenameWithPath, 'hello world\n')
+    await appendFile(filenameWithPath, 'bye world\n')
+    await rename(filenameWithPath, newFilenameWithPath)
+    const data = await readFile(newFilenameWithPath, 'utf-8')
+    console.log(data)
+  } catch (err) {
+    console.error(`[Error] There was an uncaught error: ${err}`)
+    process.exit(1)  // <----  This will stop the execution of node,
+                     //        otherwise node will continue to run.
+  }
+}
+
+fileOps()
