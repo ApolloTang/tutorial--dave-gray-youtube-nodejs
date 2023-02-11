da871c0f5184ea947619eee1141b485ce05b1a1a
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-02-11 11:50:29 -0500

    clean up

--- 02-reading-and-writing-files-with-nodejs/1131a-callback-hell.cjs
+++ 02-reading-and-writing-files-with-nodejs/1131a-callback-hell.cjs
@@ -9,11 +9,11 @@ const content = 'new content\n'
 const appendedContent = 'appended content'
 
 
-fs.writeFile(filenameWithPath, content, (err, data)=> {
+fs.writeFile(filenameWithPath, content, (err)=> {
   if (err) throw err
   console.log(`file ${filenameWithPath} written`)
 
-  fs.appendFile(filenameWithPath, appendedContent, (err, data)=> {
+  fs.appendFile(filenameWithPath, appendedContent, (err)=> {
     if (err) throw err
     console.log(`Append to ${filenameWithPath} `)
   })
