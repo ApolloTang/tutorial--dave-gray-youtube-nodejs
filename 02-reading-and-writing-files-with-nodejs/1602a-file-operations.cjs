87a2de0095c1e232b32b41b1937b03940070fb16
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-02-13 16:13:50 -0500

    add comment on appending file

--- 02-reading-and-writing-files-with-nodejs/1602a-file-operations.cjs
+++ 02-reading-and-writing-files-with-nodejs/1602a-file-operations.cjs
@@ -19,7 +19,7 @@ const fileOps = async () => {
       console.log(err)
     })
     await writeFile(filenameWithPath, 'hello world\n')
-    await appendFile(filenameWithPath, 'bye world\n')
+    await appendFile(filenameWithPath, 'bye world\n')  // appendFile will create the file if not  exit,  but will not create a dir
     await rename(filenameWithPath, newFilenameWithPath)
     const data = await readFile(newFilenameWithPath, 'utf-8')
     console.log(data)
