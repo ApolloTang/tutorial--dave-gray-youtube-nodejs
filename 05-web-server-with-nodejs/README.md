2ec81ea08f61c756930ce638f6c269aee91aa8c9
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-02-14 14:35:13 -0500

    initial: 05-web-server-with-nodejs

---
+++ 05-web-server-with-nodejs/README.md
@@ -0,0 +1,3 @@
+[How to Build a Web Server with Node JS | Node.js Tutorials for Beginners](https://www.youtube.com/watch?v=3ZAKY-CDKog&list=PL0Zuz27SZ-6PFkIxaJ6Xx_X46avTM1aYw&index=5)
+
+[github.com/gitdagray/nodejs_web_server](https://github.com/gitdagray/nodejs_web_server)
---
+++ 05-web-server-with-nodejs/wf/logEvents.js
@@ -0,0 +1,27 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import url from 'node:url'
+
+import {format} from 'date-fns'
+import {v4 as uuid} from  'uuid'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const fsPromises = fs.promises
+
+const logEvents = async message => {
+  const dateTime = `${ format(new Date(), 'yyyMMdd\tHH:mm:ss') }`
+  const logItem = `${dateTime}\t${uuid()}}\t${message}\n`
+  console.log(logItem)
+  try {
+    if (!fs.existsSync('logs')) {
+      await fsPromises.mkdir(path.join(__dirname, 'logs'))
+    }
+    await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventlog.txt'), logItem)
+  } catch (err) {
+     console.error(err)
+  }
+}
+
+
+export {logEvents}
