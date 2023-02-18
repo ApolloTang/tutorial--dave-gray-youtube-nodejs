03c92a5ba65928519f30894dd4f493d59e1facb5
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-02-18 05:09:31 -0500

    07-what-is-middleware-in-expressjs | add type txt; logout accepts

--- 07-what-is-middleware-in-expressjs/wf/server.js
+++ 07-what-is-middleware-in-expressjs/wf/server.js
@@ -125,12 +125,16 @@ app.get('/chain-123', [one, two, three])
 // catch all
 app.all(`*`, (req, res) => {
   res.status(404)
+  console.log('req.accepts(`html`): ', req.accepts(`html`))
+  console.log('req.accepts(`json`): ', req.accepts(`json`))
   if (req.accepts(`html`)) {
     res.sendFile(path.join(__dirname, 'views', '404.html'))
   } else if (req.accepts('json')) {
     // you can check this with:
     //   curl -H "Accept:application/json" http://localhost:3500/does-not-exist.json
     res.json({error: '404 NOT Found'})
+  } else {
+    res.type('txt').send('404 Not Found')
   }
 })
 
